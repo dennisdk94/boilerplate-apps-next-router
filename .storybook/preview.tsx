ba7ef8899148fa08f2de0059ac60e59f216a7059
Dennis Dyodi Kawakami <denniskawakami@gmail.com>
2024-09-29 18:08:44 -0300

    install storybook

---
+++ .storybook/preview.tsx
@@ -0,0 +1,12 @@
+// import type { Preview } from '@storybook/react'
+import React from 'react'
+import GlobalStyles from '../src/styles/global'
+
+export const decorators = [
+  (Story) => (
+    <>
+      <GlobalStyles />
+      <Story />
+    </>
+  )
+]
